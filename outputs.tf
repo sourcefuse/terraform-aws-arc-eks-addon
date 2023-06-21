79729d7738f364a0e0d516942afae4ff204daf81
Rahul Sharma <er.csrahul@gmail.com>
2023-06-21 18:50:28 +0530

    updated README.md

--- outputs.tf
+++ outputs.tf
@@ -7,16 +7,16 @@ output "cluster_id" {
 }
 
 output "cluster_endpoint" {
-  description = "The name/id of the EKS cluster"
+  description = "The endpoint of the EKS cluster"
   value       = try(module.eks.cluster_endpoint, "")
 }
 
 output "cluster_version" {
-  description = "The name/id of the EKS cluster"
+  description = "The version of the EKS cluster"
   value       = try(module.eks.cluster_version, "")
 }
 
 output "oidc_provider_arn" {
-  description = "The name/id of the EKS cluster"
+  description = "The oidc arn the EKS cluster"
   value       = try(module.eks.oidc_provider_arn, "")
 }
